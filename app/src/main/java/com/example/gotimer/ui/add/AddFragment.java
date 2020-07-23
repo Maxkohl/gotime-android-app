d42309721d2fb5fe3143c415c5f5a6e2f5d04b65
MNA Max Kohl <max_kohl@micware-na.com>
2020-07-23 17:04:05 -0400

    Began implementing AddFragment elements

--- app/src/main/java/com/example/gotimer/ui/add/AddFragment.java
+++ app/src/main/java/com/example/gotimer/ui/add/AddFragment.java
@@ -1,8 +1,41 @@
 package com.example.gotimer.ui.add;
 
-import androidx.fragment.app.Fragment;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
 
-public class AddFragment extends Fragment {
+import androidx.fragment.app.DialogFragment;
 
+import com.example.gotimer.R;
+
+public class AddFragment extends DialogFragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_add, container, false);
+    }
+
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // The only reason you might override this method when using onCreateView() is
+        // to modify any dialog characteristics. For example, the dialog includes a
+        // title by default, but your custom layout might not need it. So here you can
+        // remove the dialog title, but you must call the superclass to get the Dialog.
+        Dialog dialog = super.onCreateDialog(savedInstanceState);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        return dialog;
+    }
 
 }
