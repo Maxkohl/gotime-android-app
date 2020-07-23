280b706f5c889e406e9382274a1d756d488e2218
MNA Max Kohl <max_kohl@micware-na.com>
2020-07-23 16:10:05 -0400

    Created PopulateDbAsync to fill database with dummy data

--- app/src/main/java/com/example/gotimer/ProfileRoomDatabase.java
+++ app/src/main/java/com/example/gotimer/ProfileRoomDatabase.java
@@ -2,8 +2,11 @@ package com.example.gotimer;
 
 
 import android.content.Context;
+import android.os.AsyncTask;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.loader.content.AsyncTaskLoader;
 import androidx.room.Database;
 import androidx.room.DatabaseConfiguration;
 import androidx.room.InvalidationTracker;
@@ -51,10 +54,34 @@ public abstract class ProfileRoomDatabase extends RoomDatabase {
         return INSTANCE;
     }
 
+    //TODO Remove after testing
+    //These are for populating the database with dummy data
     private static RoomDatabase.Callback sRoomDatabaseCallback = new RoomDatabase.Callback() {
         @Override
         public void onOpen(@NonNull SupportSQLiteDatabase db) {
             super.onOpen(db);
+            new PopulateDbAsync(INSTANCE).execute();
         }
     };
+
+    private static class PopulateDbAsync extends AsyncTask<Void, Void, Void> {
+        private  final ProfileDao mDao;
+
+        Profile[] dummyProfiles = {new Profile("Profile 1", "start", "stop")};
+
+        public PopulateDbAsync(ProfileRoomDatabase db) {
+            this.mDao = db.profilesDao();
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            mDao.deleteAll();
+            for (Profile profile : dummyProfiles) {
+                mDao.insertProfile(profile);
+            }
+            return null;
+        }
+    }
+
+
 }
