2b6984e34719559ee76d73ee8c825867635fb605
MNA Max Kohl <max_kohl@micware-na.com>
2020-07-23 17:06:09 -0400

    Created TimePickerFragment for use in AddFragment

---
+++ app/src/main/java/com/example/gotimer/ui/add/TimePickerFragment.java
@@ -0,0 +1,41 @@
+package com.example.gotimer.ui.add;
+
+import android.app.Dialog;
+import android.app.TimePickerDialog;
+import android.icu.util.Calendar;
+import android.os.Build;
+import android.os.Bundle;
+
+import androidx.annotation.RequiresApi;
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.Fragment;
+
+import android.text.format.DateFormat;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TimePicker;
+
+import com.example.gotimer.R;
+
+
+public class TimePickerFragment extends DialogFragment implements TimePickerDialog.OnTimeSetListener {
+
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // Use the current time as the default values for the picker
+        final Calendar c = Calendar.getInstance();
+        int hour = c.get(Calendar.HOUR_OF_DAY);
+        int minute = c.get(Calendar.MINUTE);
+
+        // Create a new instance of TimePickerDialog and return it
+        return new TimePickerDialog(getActivity(), this, hour, minute,
+                DateFormat.is24HourFormat(getActivity()));
+    }
+
+    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+        // Do something with the time chosen by the user
+    }
+}
