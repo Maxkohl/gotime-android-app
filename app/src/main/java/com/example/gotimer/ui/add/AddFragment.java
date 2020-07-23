f1a03dc9f87407aa4be10a34ba1e0ce96d3d3a6a
MNA Max Kohl <max_kohl@micware-na.com>
2020-07-23 14:06:01 -0400

    Refactored dashboard to use as Add Timer fragment. Deleted dashboard from movile_navigation so that the fragment does not inflate when clicked

---
+++ app/src/main/java/com/example/gotimer/ui/add/AddFragment.java
@@ -0,0 +1,8 @@
+package com.example.gotimer.ui.add;
+
+import androidx.fragment.app.Fragment;
+
+public class AddFragment extends Fragment {
+
+
+}
---
+++ app/src/main/java/com/example/gotimer/ui/add/AddViewModel.java
@@ -0,0 +1,4 @@
+package com.example.gotimer.ui.add;
+
+public class AddViewModel {
+}
