7a9f61b4b69b102772f2a17f3831600212c92373
MNA Max Kohl <max_kohl@micware-na.com>
2020-07-24 16:30:54 -0400

    Created AppListAdapter shell

---
+++ app/src/main/java/com/example/gotimer/ui/appselector/AppListAdapter.java
@@ -0,0 +1,29 @@
+package com.example.gotimer.ui.appselector;
+
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class AppListAdapter extends RecyclerView.Adapter<com.example.gotimer.ui.appselector.AppListAdapter.AppsViewHolder> {
+
+
+    @NonNull
+    @Override
+    public AppsViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull AppsViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class AppsViewHolder extends RecyclerView.ViewHolder {
+    }
+}
