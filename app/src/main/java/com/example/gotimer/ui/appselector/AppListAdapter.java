94fd6e1dd4178c580b2fed8e0e5234a002251b07
MNA Max Kohl <max_kohl@micware-na.com>
2020-07-24 16:40:37 -0400

    Filled out ViewHolder and onBindViewHolder

--- app/src/main/java/com/example/gotimer/ui/appselector/AppListAdapter.java
+++ app/src/main/java/com/example/gotimer/ui/appselector/AppListAdapter.java
@@ -1,21 +1,50 @@
 package com.example.gotimer.ui.appselector;
 
+import android.content.Context;
+import android.content.pm.ApplicationInfo;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.ImageView;
+import android.widget.Switch;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.gotimer.R;
+import com.example.gotimer.entity.Profile;
+
+import org.w3c.dom.Text;
+
+import java.util.List;
+
 public class AppListAdapter extends RecyclerView.Adapter<com.example.gotimer.ui.appselector.AppListAdapter.AppsViewHolder> {
 
+    private LayoutInflater inflater;
+    private List<ApplicationInfo> mAppsInfo;
+    private Context mContext;
+
+    public AppListAdapter(Context mContext) {
+        inflater = LayoutInflater.from(mContext);
+        this.mContext = mContext;
+    }
 
     @NonNull
     @Override
     public AppsViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View itemView = inflater.inflate(R.layout.apprecycler_item, parent, false);
+        return new AppsViewHolder(itemView);
     }
 
     @Override
     public void onBindViewHolder(@NonNull AppsViewHolder holder, int position) {
+        if (mAppsInfo != null) {
+            ApplicationInfo current = mAppsInfo.get(position);
+            holder.mAppIcon.setImageResource(current.icon);
+            holder.mAppName.setText(current.packageName);
+        }
 
     }
 
@@ -25,5 +54,15 @@ public class AppListAdapter extends RecyclerView.Adapter<com.example.gotimer.ui.
     }
 
     public class AppsViewHolder extends RecyclerView.ViewHolder {
+        private ImageView mAppIcon;
+        private TextView mAppName;
+        private Switch mBlockAppSwitch;
+
+        public AppsViewHolder(@NonNull View itemView) {
+            super(itemView);
+            mAppIcon = itemView.findViewById(R.id.appIcon);
+            mAppName = itemView.findViewById(R.id.appName);
+            mBlockAppSwitch = itemView.findViewById(R.id.appBlockSwitch);
+        }
     }
 }
