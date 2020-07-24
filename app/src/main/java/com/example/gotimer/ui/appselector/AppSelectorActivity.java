b237a9469035db723e7e673ad6ecad82f6270b6f
MNA Max Kohl <max_kohl@micware-na.com>
2020-07-24 16:19:14 -0400

    Created appselector package and AppSelectorActivity

---
+++ app/src/main/java/com/example/gotimer/ui/appselector/AppSelectorActivity.java
@@ -0,0 +1,16 @@
+package com.example.gotimer.ui.appselector;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.gotimer.R;
+
+public class AppSelectorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_app_selector);
+    }
+}
