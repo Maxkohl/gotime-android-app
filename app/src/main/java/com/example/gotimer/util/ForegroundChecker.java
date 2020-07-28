c204c4c7496ffeb20040fe6b7ddaa5efc305b953
MNA Max Kohl <max_kohl@micware-na.com>
2020-07-28 10:00:45 -0400

    Added test foreground finder function in onCreate. Created start and stop service function'

--- app/src/main/java/com/example/gotimer/util/ForegroundChecker.java
+++ app/src/main/java/com/example/gotimer/util/ForegroundChecker.java
@@ -1,10 +1,16 @@
 package com.example.gotimer.util;
 
+import android.app.ActivityManager;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.IBinder;
+import android.util.Log;
 
 import androidx.annotation.Nullable;
+
+import com.example.gotimer.services.AppMonitorService;
 
 public class ForegroundChecker extends Service {
 
@@ -12,7 +18,19 @@ public class ForegroundChecker extends Service {
     @Override
     public void onCreate() {
         super.onCreate();
+        ActivityManager mActivityManager =
+                (ActivityManager) this.getSystemService(Context.ACTIVITY_SERVICE);
+
+        String mPackageName;
+        if (Build.VERSION.SDK_INT > 20) {
+            mPackageName = mActivityManager.getRunningAppProcesses().get(0).processName;
+        } else {
+            mPackageName = mActivityManager.getRunningTasks(1).get(0).topActivity.getPackageName();
+        }
+
+        Log.e("TestFunc", mPackageName);
     }
+
 
     @Override
     public void onDestroy() {
@@ -23,5 +41,13 @@ public class ForegroundChecker extends Service {
     @Override
     public IBinder onBind(Intent intent) {
         return null;
+    }
+
+    public static void start(Context context) {
+        context.startService(new Intent(context, AppMonitorService.class));
+    }
+
+    public static void stop(Context context) {
+        context.stopService(new Intent(context, AppMonitorService.class));
     }
 }
