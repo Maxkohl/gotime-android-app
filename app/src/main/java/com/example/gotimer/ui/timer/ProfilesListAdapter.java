381055d7ffea16311c00a303f5c5d82b9862377e
MNA Max Kohl <max_kohl@micware-na.com>
2020-07-28 15:00:17 -0400

    Deleted unused method

--- app/src/main/java/com/example/gotimer/ui/timer/ProfilesListAdapter.java
+++ app/src/main/java/com/example/gotimer/ui/timer/ProfilesListAdapter.java
@@ -100,6 +100,7 @@ public class ProfilesListAdapter extends RecyclerView.Adapter<com.example.gotime
         }
     }
 
+
     public boolean getProfileChanged() {
         return profileChanged;
     }
