012b23da8e1f30bc0b71a448939e3b9b0b740fe1
MNA Max Kohl <max_kohl@micware-na.com>
2020-07-29 17:30:38 -0400

    Added getCurrentApp, blockApp and openHomeScreen method to TestOverlayService

--- app/src/main/java/com/example/gotimer/services/TestOverlayService.java
+++ app/src/main/java/com/example/gotimer/services/TestOverlayService.java
@@ -1,6 +1,9 @@
 package com.example.gotimer.services;
 
+import android.app.ActivityManager;
 import android.app.Service;
+import android.app.usage.UsageStats;
+import android.app.usage.UsageStatsManager;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Canvas;
@@ -8,6 +11,7 @@ import android.graphics.Paint;
 import android.graphics.PixelFormat;
 import android.os.Build;
 import android.os.IBinder;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
@@ -22,10 +26,13 @@ import androidx.annotation.RequiresApi;
 
 import com.example.gotimer.R;
 
+import java.util.List;
+import java.util.SortedMap;
+import java.util.TreeMap;
+
 public class TestOverlayService extends Service {
 
     private WindowManager windowManager;
-    private ImageView imageView;
     private ViewGroup mViewGroup;
 
     int LAYOUT_FLAG;
@@ -64,5 +71,58 @@ public class TestOverlayService extends Service {
     @Override
     public IBinder onBind(Intent intent) {
         return null;
+    }
+
+    private String getCurrentApp() {
+        if (Build.VERSION.SDK_INT >= 21) {
+            String currentApp = null;
+            UsageStatsManager usm =
+                    (UsageStatsManager) this.getSystemService(Context.USAGE_STATS_SERVICE);
+            long time = System.currentTimeMillis();
+            List<UsageStats> applist = usm.queryUsageStats(UsageStatsManager.INTERVAL_DAILY,
+                    time - 1000 * 1000, time);
+            if (applist != null && applist.size() > 0) {
+                SortedMap<Long, UsageStats> mySortedMap = new TreeMap<>();
+                for (UsageStats usageStats : applist) {
+                    mySortedMap.put(usageStats.getLastTimeUsed(), usageStats);
+                }
+                if (mySortedMap != null && !mySortedMap.isEmpty()) {
+                    currentApp = mySortedMap.get(mySortedMap.lastKey()).getPackageName();
+                }
+            }
+            Log.e("AppMontiorServices", "Current App in foreground is: " + currentApp);
+
+            return currentApp;
+
+        } else {
+            //For anything lower than Lollipop
+            ActivityManager manager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+            String mm = (manager.getRunningTasks(1).get(0)).topActivity.getPackageName();
+            Log.e("AppMontiorService", "Current App in foreground is: " + mm);
+            return mm;
+        }
+    }
+
+    public boolean blockApp() {
+        String currentAppProcess = getCurrentApp();
+        if ("com.instagram.android".equals(currentAppProcess)) {
+            openHomeScreen();
+            //TODO add a slight delay here?
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return true;
+        }
+        return false;
+    }
+
+    public boolean openHomeScreen() {
+        Intent startHomescreen = new Intent(Intent.ACTION_MAIN);
+        startHomescreen.addCategory(Intent.CATEGORY_HOME);
+        startHomescreen.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(startHomescreen);
+        return true;
     }
 }
