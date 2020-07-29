ee8a962df370d8afeb2bbab85692e2476d54f5f6
MNA Max Kohl <max_kohl@micware-na.com>
2020-07-29 17:59:46 -0400

    Experimenting with overlay block

--- app/src/main/java/com/example/gotimer/services/TestOverlayService.java
+++ app/src/main/java/com/example/gotimer/services/TestOverlayService.java
@@ -10,7 +10,9 @@ import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.PixelFormat;
 import android.os.Build;
+import android.os.Handler;
 import android.os.IBinder;
+import android.os.Looper;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
@@ -34,6 +36,8 @@ public class TestOverlayService extends Service {
 
     private WindowManager windowManager;
     private ViewGroup mViewGroup;
+    Handler handler;
+    Runnable runnableCode;
 
     int LAYOUT_FLAG;
 
@@ -41,23 +45,15 @@ public class TestOverlayService extends Service {
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         Toast.makeText(this, "Test Service Started", Toast.LENGTH_SHORT).show();
-        LAYOUT_FLAG = WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY;
-        LayoutInflater inflater = (LayoutInflater)getSystemService(LAYOUT_INFLATER_SERVICE);
-        windowManager = (WindowManager) getSystemService(WINDOW_SERVICE);
-        mViewGroup = (ViewGroup) inflater.inflate(R.layout.overlay_layout, null);
-
-        final WindowManager.LayoutParams params = new WindowManager.LayoutParams(
-                WindowManager.LayoutParams.WRAP_CONTENT,
-                WindowManager.LayoutParams.WRAP_CONTENT,
-                LAYOUT_FLAG,
-                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE | WindowManager.LayoutParams.FLAG_WATCH_OUTSIDE_TOUCH | WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
-                PixelFormat.TRANSLUCENT);
-        params.gravity = Gravity.TOP | Gravity.LEFT;
-
-
-        params.x = 0;
-        params.y = 150;
-        windowManager.addView(mViewGroup, params);
+        handler = new Handler(Looper.getMainLooper());
+        runnableCode = new Runnable() {
+            @Override
+            public void run() {
+                blockApp();
+                handler.postDelayed(runnableCode, 1000);
+            }
+        };
+        handler.post(runnableCode);
         return super.onStartCommand(intent, flags, startId);
     }
 
@@ -106,7 +102,7 @@ public class TestOverlayService extends Service {
     public boolean blockApp() {
         String currentAppProcess = getCurrentApp();
         if ("com.instagram.android".equals(currentAppProcess)) {
-            openHomeScreen();
+            displayOverlay();
             //TODO add a slight delay here?
             try {
                 Thread.sleep(1000);
@@ -124,5 +120,25 @@ public class TestOverlayService extends Service {
         startHomescreen.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         startActivity(startHomescreen);
         return true;
+    }
+
+    public void displayOverlay() {
+        LAYOUT_FLAG = WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY;
+        LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
+        windowManager = (WindowManager) getSystemService(WINDOW_SERVICE);
+        mViewGroup = (ViewGroup) inflater.inflate(R.layout.overlay_layout, null);
+
+        final WindowManager.LayoutParams params = new WindowManager.LayoutParams(
+                WindowManager.LayoutParams.WRAP_CONTENT,
+                WindowManager.LayoutParams.WRAP_CONTENT,
+                LAYOUT_FLAG,
+                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE | WindowManager.LayoutParams.FLAG_WATCH_OUTSIDE_TOUCH | WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+                PixelFormat.TRANSLUCENT);
+        params.gravity = Gravity.TOP | Gravity.LEFT;
+
+
+        params.x = 0;
+        params.y = 150;
+        windowManager.addView(mViewGroup, params);
     }
 }
