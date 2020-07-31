102dcd56d46e44bc3fe7540cde7ef0d8239b6cfe
MNA Max Kohl <max_kohl@micware-na.com>
2020-07-31 15:55:50 -0400

    Created init method, show/hide clearButton methods, TextWatcher

--- app/src/main/java/com/example/gotimer/customviews/EditTextWithClear.java
+++ app/src/main/java/com/example/gotimer/customviews/EditTextWithClear.java
@@ -1,20 +1,64 @@
 package com.example.gotimer.customviews;
 
 import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.AttributeSet;
 
 import androidx.appcompat.widget.AppCompatEditText;
+import androidx.core.content.res.ResourcesCompat;
+
+import com.example.gotimer.R;
 
 public class EditTextWithClear extends AppCompatEditText {
+    Drawable mClearButtonImage;
+
     public EditTextWithClear(Context context) {
         super(context);
+        init();
     }
 
     public EditTextWithClear(Context context, AttributeSet attrs) {
         super(context, attrs);
+        init();
     }
 
     public EditTextWithClear(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    private void init() {
+        mClearButtonImage = ResourcesCompat.getDrawable(getResources(), R.drawable.ic_cancel, null);
+        addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+    }
+
+    private void showClearButton() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+            setCompoundDrawablesRelativeWithIntrinsicBounds(null, null, mClearButtonImage, null);
+        }
+    }
+
+    private void hideClearButton() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+            setCompoundDrawablesRelativeWithIntrinsicBounds(null, null, null, null);
+        }
     }
 }
