02bc7142a4714275502f4fc9ebaca76e06d84e2e
MNA Max Kohl <max_kohl@micware-na.com>
2020-07-31 16:09:44 -0400

    Replaced EditText in fragment_add with EditTextWithClear custom view

--- app/src/main/java/com/example/gotimer/customviews/EditTextWithClear.java
+++ app/src/main/java/com/example/gotimer/customviews/EditTextWithClear.java
@@ -6,7 +6,10 @@ import android.os.Build;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.widget.AppCompatEditText;
 import androidx.core.content.res.ResourcesCompat;
 
@@ -35,12 +38,12 @@ public class EditTextWithClear extends AppCompatEditText {
         addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-                
+
             }
 
             @Override
             public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
+                showClearButton();
             }
 
             @Override
@@ -48,7 +51,42 @@ public class EditTextWithClear extends AppCompatEditText {
 
             }
         });
+
+        setOnTouchListener(new OnTouchListener() {
+            @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR1)
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                if ((getCompoundDrawablesRelative()[2] != null)) {
+                    boolean isClearButtonClicked = false;
+                    float clearButtonStart;
+                    clearButtonStart = (getWidth() - getPaddingEnd()
+                            - mClearButtonImage.getIntrinsicWidth());
+                    if (motionEvent.getX() > clearButtonStart) {
+                        isClearButtonClicked = true;
+                    }
+                    if (isClearButtonClicked) {
+                        if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
+                            mClearButtonImage = ResourcesCompat.getDrawable(getResources(),
+                                    R.drawable.ic_cancel, null);
+                            showClearButton();
+                        }
+                        if (motionEvent.getAction() == MotionEvent.ACTION_UP) {
+                            mClearButtonImage = ResourcesCompat.getDrawable(getResources(),
+                                    R.drawable.ic_cancel, null);
+                            getText().clear();
+                            hideClearButton();
+                            return true;
+                        }
+                    } else {
+                        return false;
+                    }
+                }
+                return false;
+            }
+        });
+
     }
+
 
     private void showClearButton() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
