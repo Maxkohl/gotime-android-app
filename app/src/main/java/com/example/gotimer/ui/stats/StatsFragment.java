2130cebabde8bef558376708b5e4af4143a6cd69
MNA Max Kohl <max_kohl@micware-na.com>
2020-08-05 15:10:14 -0400

    Added dummy data AnyChart to stats for presentation purposes

--- app/src/main/java/com/example/gotimer/ui/stats/StatsFragment.java
+++ app/src/main/java/com/example/gotimer/ui/stats/StatsFragment.java
@@ -12,7 +12,21 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.anychart.AnyChart;
+import com.anychart.AnyChartView;
+import com.anychart.chart.common.dataentry.DataEntry;
+import com.anychart.chart.common.dataentry.ValueDataEntry;
+import com.anychart.charts.Cartesian;
+import com.anychart.charts.Pie;
+import com.anychart.core.cartesian.series.Column;
+import com.anychart.enums.Anchor;
+import com.anychart.enums.HoverMode;
+import com.anychart.enums.Position;
+import com.anychart.enums.TooltipPositionMode;
 import com.example.gotimer.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class StatsFragment extends Fragment {
 
@@ -23,13 +37,45 @@ public class StatsFragment extends Fragment {
         statsViewModel =
                 ViewModelProviders.of(this).get(StatsViewModel.class);
         View root = inflater.inflate(R.layout.fragment_stats, container, false);
-        final TextView textView = root.findViewById(R.id.text_notifications);
-        statsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+
+        AnyChartView anyChartView = root.findViewById(R.id.any_chart_view);
+
+        Cartesian cartesian = AnyChart.column();
+
+        List<DataEntry> data = new ArrayList<>();
+        data.add(new ValueDataEntry("Instagram", 80540));
+        data.add(new ValueDataEntry("Facebook", 94190));
+        data.add(new ValueDataEntry("Netflix", 102610));
+        data.add(new ValueDataEntry("Tiktok", 110430));
+        data.add(new ValueDataEntry("Reddit", 128000));
+        data.add(new ValueDataEntry("Hulu", 143760));
+        data.add(new ValueDataEntry("Subby", 170670));
+
+        Column column = cartesian.column(data);
+
+        column.tooltip()
+                .titleFormat("{%X}")
+                .position(Position.CENTER_BOTTOM)
+                .anchor(Anchor.CENTER_BOTTOM)
+                .offsetX(0d)
+                .offsetY(5d)
+                .format("{%Value}{groupsSeparator: }");
+
+        cartesian.animation(true);
+        cartesian.title("Productivity Hours per Day");
+
+        cartesian.yScale().minimum(0d);
+
+        cartesian.yAxis(0).labels().format("{%Value}{groupsSeparator: }");
+
+        cartesian.tooltip().positionMode(TooltipPositionMode.POINT);
+        cartesian.interactivity().hoverMode(HoverMode.BY_X);
+
+        cartesian.xAxis(0).title("Hours");
+        cartesian.yAxis(0).title("App");
+
+        anyChartView.setChart(cartesian);
+
         return root;
     }
 }
